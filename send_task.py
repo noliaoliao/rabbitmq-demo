ad4288748710594bb7cedf01d30a9ad9fc6eb60e
noliaoliao <noliaoliao@gmail.com>
2016-04-22 17:17:07 +0800

    test work queue.

---
+++ send_task.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+"""
+    the send
+"""
+
+import pika
+import sys
+
+if __name__ == "__main__":
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    channel = connection.channel()
+    message = ''.join(sys.argv[1:]) or "hello,world!"
+    channel.queue_declare(queue='task_queue')
+    #channel.basic_publish(exchange='',routing_key='hello',body='hello world!')
+    channel.basic_publish(exchange='',
+                        routing_key = 'task_queue',
+                        body = message,
+                        properties = pika.BasicProperties(delivery_mode = 2,))
+    print 'send hello world!'
+    connection.close()
+
+
