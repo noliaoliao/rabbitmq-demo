9f7cd1c4b5bca66027671f86b719b3bbee73e6f2
noliaoliao <noliaoliao@gmail.com>
2016-04-22 21:14:41 +0800

    enable Message acknowledgment,Message durability and Fair Dispatch

--- send_task.py
+++ send_task.py
@@ -11,9 +11,10 @@ if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     message = ''.join(sys.argv[1:]) or "hello,world!"
-    print message
-    channel.queue_declare(queue='task_queue')
-    #channel.basic_publish(exchange='',routing_key='hello',body='hello world!')
+    # declare queue as durable to make sure the RabbitMQ
+    # will never lose our queue
+    channel.queue_declare(queue='task_queue', durable = True)
+    # channel.basic_publish(exchange='',routing_key='hello',body='hello world!')
     channel.basic_publish(exchange='',
                         routing_key = 'task_queue',
                         body = message,
--- worker.py
+++ worker.py
@@ -8,13 +8,19 @@ def callback(ch, method, properties, body):
     time.sleep(body.count(b'.'))
     print 'done'
     #print method.delivery_tag
-    #ch.basic_ack(delivery_tag = method.delivery_tag)
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+    channel.basic_consume(callback, queue = 'task_queue')
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='task_queue')
-    channel.basic_consume(callback, queue='task_queue', no_ack=True)
+    channel.queue_declare(queue='task_queue', durable = True)
+    #no_ack is setted to False by default 
+    #so Message acknowledgments are turned on
+    #in the callback function we need send a proper
+    #acknowledgment to RabbitMQ server when the work is done.
+    channel.basic_qos(prefetch_count = 1)
+    channel.basic_consume(callback, queue='task_queue', no_ack=False)
 
     print 'wating the message...'
 
