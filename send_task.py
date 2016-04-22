2384c574681033e0f3ba2f736015786958cbb849
noliaoliao <noliaoliao@gmail.com>
2016-04-22 17:44:04 +0800

    add worker.py

--- send_task.py
+++ send_task.py
@@ -11,13 +11,14 @@ if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
     message = ''.join(sys.argv[1:]) or "hello,world!"
+    print message
     channel.queue_declare(queue='task_queue')
     #channel.basic_publish(exchange='',routing_key='hello',body='hello world!')
     channel.basic_publish(exchange='',
                         routing_key = 'task_queue',
                         body = message,
                         properties = pika.BasicProperties(delivery_mode = 2,))
-    print 'send hello world!'
+    print 'send ', message
     connection.close()
 
 
--- worker.py
+++ worker.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
 import pika
+import time
 
 def callback(ch, method, properties, body):
     print 'received %r' %body
+    time.sleep(body.count(b'.'))
+    print 'done'
+    #print method.delivery_tag
+    #ch.basic_ack(delivery_tag = method.delivery_tag)
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='hello')
-    channel.basic_consume(callback, queue='hello', no_ack=True)
+    channel.queue_declare(queue='task_queue')
+    channel.basic_consume(callback, queue='task_queue', no_ack=True)
 
     print 'wating the message...'
 
