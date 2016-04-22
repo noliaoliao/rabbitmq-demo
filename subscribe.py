0503973c051883d96c6c3b620a752c1a7f9b8aa7
noliaoliao <noliaoliao@gmail.com>
2016-04-22 22:58:44 +0800

    publish/subcribe

--- subscribe.py
+++ subscribe.py
@@ -5,22 +5,21 @@ import time
 
 def callback(ch, method, properties, body):
     print 'received %r' %body
-    time.sleep(body.count(b'.'))
-    print 'done'
-    #print method.delivery_tag
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-    channel.basic_consume(callback, queue = 'task_queue')
 
 if __name__ == "__main__":
     connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-    channel.queue_declare(queue='task_queue', durable = True)
+    res = channel.queue_declare(exclusive = True)
+    queue_name = res.method.queue
+
+    channel.queue_bind(exchange='logs',queue=queue_name)
+
     #no_ack is setted to False by default 
     #so Message acknowledgments are turned on
     #in the callback function we need send a proper
     #acknowledgment to RabbitMQ server when the work is done.
-    channel.basic_qos(prefetch_count = 1)
-    channel.basic_consume(callback, queue='task_queue', no_ack=False)
+    #channel.basic_qos(prefetch_count = 1)
+    channel.basic_consume(callback, queue=queue_name, no_ack=True)
 
     print 'wating the message...'
 
